0f3558377f9fb1d515311d36b4a7bfb32a2b8fdf
erick moses <moseserickomondi@gmail.com>
2025-08-28 09:17:01 +0300

    made changes to the employee.py file

--- lib/employee.py
+++ lib/employee.py
@@ -135,15 +135,13 @@ class Employee:
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
 
-        # Check the dictionary for  existing instance using the row's primary key
         employee = cls.all.get(row[0])
+
         if employee:
-            # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -186,5 +184,6 @@ class Employee:
         return cls.instance_from_db(row) if row else None
 
     def reviews(self):
-        """Return list of reviews associated with current employee"""
-        pass
+        """Return a list of reviews associated with the current employee"""
+        from review import Review  # Import here to avoid circular imports
+        return [review for review in Review.all.values() if review.employee_id == self.id]
